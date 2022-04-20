5a37e0f2d3f3f5af1c6e190f93981b3d521a5a48
Samruddhi <samruddhipathe@gmail.com>
2022-04-20 20:30:25 +0530

    Commited the initial creation of project

---
+++ src/test/java/com/javaconcepts/JavaConceptsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.javaconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
